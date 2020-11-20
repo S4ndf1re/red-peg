b70ae4017fc7c39e320bc3262dda3e23441c4b10
VayuDev <vayudev@protonmail.com>
2020-11-21 00:16:35 +0100

    Parser: Add validation for some expressions

--- src/parser.rs
+++ src/parser.rs
@@ -1,24 +1,36 @@
 use crate::tokenizer::CodeTokenizer;
-use std::fmt;
+use regex::Regex;
 use std::collections::HashMap;
+use std::fmt;
 
 pub struct ParsingResult {}
+
+pub struct ParsingInformation<'a> {
+    rules: &'a HashMap<String, Box<dyn ParsingExpression>>,
+    tokenizer: &'a mut CodeTokenizer,
+}
 
 pub trait ParsingExpression {
     fn matches(&self, tokenizer: &mut CodeTokenizer) -> ParsingResult;
     fn dump(&self) -> String {
         return String::from("ParsingExpression");
     }
+    fn validate(&self, tokenizer: &mut ParsingInformation) -> bool;
+}
+
+pub enum TerminalType {
+    SIMPLE(String),
+    REGEX(Regex),
 }
 
 pub struct TerminalParsingExpression {
-    name: String,
+    content: TerminalType,
 }
 
 impl TerminalParsingExpression {
     pub fn new(p_name: &str) -> Box<dyn ParsingExpression> {
         Box::new(TerminalParsingExpression {
-            name: String::from(p_name),
+            content: TerminalType::SIMPLE(String::from(p_name)),
         })
     }
 }
@@ -28,7 +40,29 @@ impl ParsingExpression for TerminalParsingExpression {
         ParsingResult {}
     }
     fn dump(&self) -> String {
-        return String::from(format!("'{}'", self.name));
+        match &self.content {
+            TerminalType::SIMPLE(str) => {
+                let mut ret = String::from('\'');
+                ret.push_str(str);
+                ret.push('\'');
+                ret
+            }
+            TerminalType::REGEX(reg) => {
+                reg.to_string()
+            }
+        }
+    }
+
+    fn validate(&self, info: &mut ParsingInformation) -> bool {
+        match &self.content {
+            TerminalType::SIMPLE(str) => {
+                let token = info.tokenizer.next_token();
+                token.content == *str
+            }
+            TerminalType::REGEX(reg) => {
+                todo!()
+            }
+        }
     }
 }
 
@@ -50,6 +84,9 @@ impl ParsingExpression for NonTerminalParsingExpression {
     }
     fn dump(&self) -> String {
         return String::from(format!("{}", self.name));
+    }
+    fn validate(&self, mut info: &mut ParsingInformation) -> bool {
+        return info.rules.get(&self.name).expect("No rule for this non-terminal!").validate(&mut info);
     }
 }
 
@@ -80,6 +117,14 @@ impl ParsingExpression for SequenceParsingExpression {
             i += 1;
         }
         return ret;
+    }
+    fn validate(&self, info: &mut ParsingInformation) -> bool {
+        for child in &self.children {
+            if !child.validate(info) {
+                return false;
+            }
+        }
+        return true;
     }
 }
 
@@ -112,6 +157,16 @@ impl ParsingExpression for ChoiceParsingExpresion {
         ret.push_str(")");
         return ret;
     }
+    fn validate(&self, info: &mut ParsingInformation) -> bool {
+        for child in &self.children {
+            info.tokenizer.push_state();
+            if child.validate(info) {
+                return true;
+            }
+            info.tokenizer.pop_state();
+        }
+        return false;
+    }
 }
 pub struct Parser {
     rules: HashMap<String, Box<dyn ParsingExpression>>,
@@ -119,23 +174,30 @@ pub struct Parser {
 
 impl Parser {
     pub fn new() -> Parser {
-        Parser { rules: HashMap::new() }
+        Parser {
+            rules: HashMap::new(),
+        }
     }
-    pub fn add_rule(&mut self, left_side : &str, right_side : Box<dyn ParsingExpression>) {
+    pub fn add_rule(&mut self, left_side: &str, right_side: Box<dyn ParsingExpression>) {
         self.rules.insert(String::from(left_side), right_side);
     }
     pub fn validate(&self, start_non_terminal: &str, code: &str) -> bool {
         let mut tokenizer = CodeTokenizer::new(code);
-        let rule = self.rules.get(non_terminal).expect("No matching rule for non-terminal!");
-        rule.validate(tokenizer, &self.rules);
-        true
+        let rule = self
+            .rules
+            .get(start_non_terminal)
+            .expect("No matching rule for non-terminal!");
+        let rule_result = rule.validate(&mut ParsingInformation {
+            rules: &self.rules,
+            tokenizer: &mut tokenizer,
+        });
+        rule_result && tokenizer.is_empty()
     }
 }
 
-
 impl fmt::Display for Parser {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        let mut res : fmt::Result = fmt::Result::Ok(());
+        let mut res: fmt::Result = fmt::Result::Ok(());
         for (left_side, right_side) in &self.rules {
             res = res.and(write!(f, "{} -> {}", left_side, right_side.dump()));
         }
--- tests/parser.rs
+++ tests/parser.rs
@@ -1,47 +1,101 @@
 #[cfg(test)]
-mod stringify {
+mod parser {
     use red_peg::parser::*;
     #[test]
     fn stringify_choice_sequence_terminal() {
         let mut p = Parser::new();
-        p.add_rule("Start", ChoiceParsingExpresion::new(vec![
-            SequenceParsingExpression::new(vec![
-                TerminalParsingExpression::new("A"),
-                TerminalParsingExpression::new("B"),
-                TerminalParsingExpression::new("C"),
+        p.add_rule(
+            "Start",
+            ChoiceParsingExpresion::new(vec![
+                SequenceParsingExpression::new(vec![
+                    TerminalParsingExpression::new("A"),
+                    TerminalParsingExpression::new("B"),
+                    TerminalParsingExpression::new("C"),
+                ]),
+                TerminalParsingExpression::new("D"),
             ]),
-            TerminalParsingExpression::new("D"),
-        ]));
+        );
         assert_eq!(format!("{}", p), "Start -> ('A' 'B' 'C' | 'D')");
         let mut p = Parser::new();
-        p.add_rule("XYZ", SequenceParsingExpression::new(vec![
-            ChoiceParsingExpresion::new(vec![
-                TerminalParsingExpression::new("A"),
-                TerminalParsingExpression::new("B"),
-                TerminalParsingExpression::new("C"),
+        p.add_rule(
+            "XYZ",
+            SequenceParsingExpression::new(vec![
+                ChoiceParsingExpresion::new(vec![
+                    TerminalParsingExpression::new("A"),
+                    TerminalParsingExpression::new("B"),
+                    TerminalParsingExpression::new("C"),
+                ]),
+                TerminalParsingExpression::new("D"),
             ]),
-            TerminalParsingExpression::new("D"),
-        ]));
+        );
         assert_eq!(format!("{}", p), "XYZ -> ('A' | 'B' | 'C') 'D'");
     }
 
     #[test]
     fn stringify_non_terminal() {
         let mut p = Parser::new();
-        p.add_rule("XYZ", SequenceParsingExpression::new(vec![
+        p.add_rule(
+            "XYZ",
+            SequenceParsingExpression::new(vec![
                 ChoiceParsingExpresion::new(vec![
                     NonTerminalParsingExpression::new("A"),
                     TerminalParsingExpression::new("B"),
                     NonTerminalParsingExpression::new("C"),
                 ]),
                 TerminalParsingExpression::new("D"),
-            ]));
+            ]),
+        );
         assert_eq!(format!("{}", p), "XYZ -> (A | 'B' | C) 'D'");
     }
     #[test]
     fn validate() {
         let mut parser = Parser::new();
-        parser.add_rule("Start",TerminalParsingExpression::new("a"));
+        parser.add_rule("Start", TerminalParsingExpression::new("a"));
         assert!(parser.validate("Start", "a"));
+        assert!(!parser.validate("Start", "b"));
+
+
+        let mut parser = Parser::new();
+        parser.add_rule("Start", SequenceParsingExpression::new(vec![
+            TerminalParsingExpression::new("a"),
+            TerminalParsingExpression::new("b")
+        ]));
+        assert!(parser.validate("Start", "a b"));
+        assert!(!parser.validate("Start", "a a"));
+        assert!(!parser.validate("Start", "b a a"));
+
+
+        let mut parser = Parser::new();
+        parser.add_rule("Start", SequenceParsingExpression::new(vec![
+            ChoiceParsingExpresion::new(vec![
+                TerminalParsingExpression::new("a"),
+                TerminalParsingExpression::new("b")
+            ]),
+            TerminalParsingExpression::new("c")
+        ]));
+        assert!(parser.validate("Start", "a c"));
+        assert!(parser.validate("Start", "b c"));
+        assert!(!parser.validate("Start", "a b"));
+
+        let mut parser = Parser::new();
+        parser.add_rule("Start", SequenceParsingExpression::new(vec![
+            ChoiceParsingExpresion::new(vec![
+                TerminalParsingExpression::new("a"),
+                NonTerminalParsingExpression::new("Second"),
+            ]),
+            TerminalParsingExpression::new("c")
+        ]));
+        parser.add_rule("Second",
+            ChoiceParsingExpresion::new(vec![
+                SequenceParsingExpression::new(vec![
+                    TerminalParsingExpression::new("c"),
+                    TerminalParsingExpression::new("d")
+                ]),
+                TerminalParsingExpression::new("b")
+            ]));
+        assert!(parser.validate("Start", "a c"));
+        assert!(parser.validate("Start", "b c"));
+        assert!(parser.validate("Start", "c d c"));
+        assert!(!parser.validate("Start", "c d b c "));
     }
 }
